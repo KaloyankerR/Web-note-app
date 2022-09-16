75f47c746ec5144554277857c22b8471c486830b
KaloyankerR <kaloyankulov2003kk@gmail.com>
2022-09-16 15:07:38 +0200

    Adde update function

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from fileinput import filename
 import json
-import os.path
 from werkzeug.utils import secure_filename
 from flask import Flask, render_template, request, redirect, url_for, flash, abort, session, jsonify
 
@@ -14,22 +12,50 @@ def get_json_data():
     return data
 
 
+def update_json_data(json_key, json_value):
+    data = get_json_data()
+    data[json_key] = json_value
+
+    with open('data.json', 'w') as f:
+        json.dump(data, f)
+
+
 @app.route('/', methods=['POST', 'GET'])
 def home():
     if request.method == 'POST':
         note_title = request.form['note_title']
         note = request.form['note']
         res = {note_title: note}
-        with open('data.json', 'r') as f:
-            data = json.load(f)
-
+        # with open('data.json', 'r') as f:
+        #     data = json.load(f)
+        data = get_json_data()
         data.update(res)
+
         with open('data.json', 'w') as f:
             json.dump(data, f)
 
         return render_template('index.html', data=get_json_data())
     else:
         return render_template('index.html', data=get_json_data())
+
+
+@app.route('/update/<note>', methods=['POST', 'GET'])
+def update_note(note):
+    # data = get_json_data()
+    description = request.form['description'].value
+
+    if request.method == 'POST':
+        # new_note_title = request.form['note_title']
+        new_note = request.form['note']
+        update_json_data(json_key=note, json_value=new_note)
+        
+        # data[note] = new_note
+        # with open('data.json', 'w') as f:
+        #     json.dump(data, f)
+
+        return redirect('/')
+    else:
+        return render_template("update.html", note=note, description=description)
 
 
 if __name__ == '__main__':
--- demo.py
+++ demo.py
@@ -3,19 +3,27 @@ import json
 # with open("data_file.json", "w") as write_file:
 #   json.dump(data, write_file)
 
-with open("data_file.json", "r") as f:
-    data = json.load(f)
+# with open("data_file.json", "r") as f:
+#     data = json.load(f)
 
-print(data)
-a = {'tosho': 'peshov'}
-data.update(a)
-print(data)
-b = {'tisho': 'kokov'}
-data.update(b)
-print(data)
-c = {'testosho': 'test'}
-data.update(c)
-print(data)
+# --------------------------------
+# print(data)
+# a = {'tosho': 'peshov'}
+# data.update(a)
+# print(data)
+# b = {'tisho': 'kokov'}
+# data.update(b)
+# print(data)
+# c = {'testosho': 'test'}
+# data.update(c)
+# print(data)
 
-with open('data_file.json', 'w') as f:
-    json.dump(data, f)
+# with open('data_file.json', 'w') as f:
+#     json.dump(data, f)
+
+a = {'a': 'a1',
+     'b': 'b1',
+     'c': 'c1'}
+
+for i in a:
+    print(a[i])
